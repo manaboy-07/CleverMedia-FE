32bc5601b21bc22d2f3bb903161a3d951a8eaa10
Dubjay18 <jejeniyi7@gmail.com>
2022-06-24 00:11:14 +0100

    components folder added

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,11 +1,11 @@
 import React from "react";
 
 function HomePage() {
-  return <div className="bg-base-100 text-slate-300 min-h-screen">
-    <div className="flex justify-center align-center">
-      
+  return (
+    <div className="bg-base-100 text-slate-300 min-h-screen">
+      <div className="flex justify-center align-center"></div>
     </div>
-  </div>;
+  );
 }
 
 export default HomePage;
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,24 +3,23 @@ module.exports = {
     themes: [
       {
         mytheme: {
-        
-"primary": "#3730a3",
-        
-"secondary": "#1e3a8a",
-        
-"accent": "#1FB2A6",
-        
-"neutral": "#1f2937",
-        
-"base-100": "#111827",
-        
-"info": "#e11d48",
-        
-"success": "#36D399",
-        
-"warning": "#FBBD23",
-        
-"error": "#F87272",
+          primary: "#3730a3",
+
+          secondary: "#1e3a8a",
+
+          accent: "#1FB2A6",
+
+          neutral: "#1f2937",
+
+          "base-100": "#111827",
+
+          info: "#e11d48",
+
+          success: "#36D399",
+
+          warning: "#FBBD23",
+
+          error: "#F87272",
         },
       },
     ],
@@ -28,4 +27,4 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   plugins: ["@emotion", require("daisyui")],
   //...
-}  
+};
