e7653222083a565431aff4b419b8908f68f973af
manaboy-07 <manassehoruebor@gmail.com>
2022-06-24 23:59:12 +0100

    saved changes

--- src/components/landingpage/Navbar.js
+++ src/components/landingpage/Navbar.js
@@ -38,8 +38,8 @@ const Navbar = () => {
           <svg class="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z"/></svg>
         </a>
         <ul className ="p-2  text-success">
-          <li><a href="#">Submenu 1</a></li>
-          <li><a href="#">Submenu 2</a></li>
+          <li><a href="#">item 1 </a></li>
+          <li><a href="#">item 2</a></li>
           </ul>
             </li>
           <li><a href="#" className=" text-success">Contact</a></li>
