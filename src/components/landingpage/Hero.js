4fd6bf28bedd36b9d4fcbf482c8f5798f8c453d9
Dubjay18 <jejeniyi7@gmail.com>
2022-06-24 21:13:21 +0100

    fix:components folder structure

--- src/components/landingpage/Hero.js
+++ src/components/landingpage/Hero.js
@@ -1,22 +1,27 @@
 import React from "react";
-import social from "../images/connected.svg"
-
-
+import social from "../../images/connected.svg";
 
 const Hero = () => {
-    return(
-        <div className="flex flex-row items-center justify-evenly space-y-6 my-12 p-5 ml-32 md:flex-column-reverse">
-           <div>
-             <h2 className="text-success text-2xl">New Platform</h2>
-             <h1 className="font-bold text-6xl">Your next <p> social life . </p></h1>
-             <h3 className="text-xl my-12">Your new social media platform <p>with stuff that actually matters!!!</p></h3>
-             <button className="text-xl text-info bg-base-100 outline-info border-4 border-info  p-3 ">Get started</button>
-           </div>
-           <div className="mx-22">
-             <img src={social} alt="smth" className="z-20"/>
-           </div>
-        </div>
-    )
-}
+  return (
+    <div className="flex flex-row items-center justify-evenly space-y-6 my-12 p-5 ml-32 md:flex-column-reverse">
+      <div>
+        <h2 className="text-success text-2xl">New Platform</h2>
+        <h1 className="font-bold text-6xl">
+          Your next <p> social life . </p>
+        </h1>
+        <h3 className="text-xl my-12">
+          Your new social media platform{" "}
+          <p>with stuff that actually matters!!!</p>
+        </h3>
+        <button className="text-xl text-info bg-base-100 outline-info border-4 border-info  p-3 ">
+          Get started
+        </button>
+      </div>
+      <div className="mx-22">
+        <img src={social} alt="smth" className="z-20" />
+      </div>
+    </div>
+  );
+};
 
-export default Hero
+export default Hero;
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Hero from "../components/landingpage/Hero";
-import Navbar from "../components/mainpage/Navbar";
+import Navbar from "../components/landingpage/Navbar";
 function HomePage() {
   return (
     <div className="bg-base-100 text-slate-200  min-h-screen">
