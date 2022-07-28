08210913a957839b72a676b21595d67f95f861cb
manaboy-07 <manassehoruebor@gmail.com>
2022-07-28 16:49:09 +0100

    implemented redux and added text in the ReadMe

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
+/** @format */
+
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import reportWebVitals from "./reportWebVitals";
+import store from "./redux/store";
+import { Provider } from "react-redux";
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
