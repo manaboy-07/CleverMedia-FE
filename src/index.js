36278e41822af473112ff64c7e366b415a8c1539
manaboy-07 <manassehoruebor@gmail.com>
2022-08-06 10:42:49 +0100

    fixed location of redux store

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import reportWebVitals from "./reportWebVitals";
-import store from "./redux/store";
+import store from "../src/pages/redux/store"
 import { Provider } from "react-redux";
 ReactDOM.render(
   <React.StrictMode>
